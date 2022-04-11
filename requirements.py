71be9650b6c6e9e5fb8184fbade190f1383398ea
Tahg <103221002+T4hg@users.noreply.github.com>
2022-04-11 11:39:09 +0200

    Install Requirements

---
+++ requirements.py
@@ -0,0 +1,29 @@
+import os
+try:
+    os.system("pip install colorama")
+except:
+    pass
+try:
+    os.system("pip install base64")
+except:
+    pass
+try:
+    os.system("pip install requests")
+except:
+    pass
+try:
+    os.system("pip install datetime")
+except:
+    pass
+try:
+    os.system("pip install tk")
+except:
+    pass
+try:
+    os.system("pip install ctypes")
+except:
+    pass
+try:
+    os.system("pip install random")
+except:
+    pass
